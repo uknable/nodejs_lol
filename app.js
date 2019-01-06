a2e0da2484555441d7bd2bbe733f94a76715e7ae
uknable <uknable@hotmail.com>
2019-01-06 19:18:29 +1100

    i still dunno wtf

--- app.js
+++ app.js
@@ -4,39 +4,48 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
-let apiKey = 'RGAPI-b7e3d714-9faf-4201-8cef-e49b34a391db';
+let apiKey = 'RGAPI-deed69ff-63db-40a2-a22e-a42e293f1db5';
+let urlHost = "https://oc1.api.riotgames.com";
+let testQuery = "?endIndex=5";
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-app.post('/', function(req, res) {
+app.post('/', (req, res) => {
 
   let summName = req.body.summonerName;
-  let urlSumm = `https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summName}?api_key=${apiKey}`;
+  let urlSumm = `${urlHost}/lol/summoner/v4/summoners/by-name/${summName}?api_key=${apiKey}`;
 
-  request(urlSumm, function(err, resp, body) {
+  request(urlSumm, (err, resp, body) => {
     if(err) {
-    
-      res.render('index', {summonerName: null, matchList:null, error: 'Error, try again'});
-    
+
+      res.render('home', {summonerName: null, matchList:null, error: 'summoner not found'});
+
     } else {
 
       let jsonBody = JSON.parse(body);
       let accId = jsonBody.accountId;
-      let urlMatchList = `https://oc1.api.riotgames.com/lol/match/v4/matchlists/by-account/${accId}?endIndex=5&api_key=${apiKey}`;
+      let urlMatchList = `${urlHost}/lol/match/v4/matchlists/by-account/${accId}${testQuery}&api_key=${apiKey}`;
 
-      request(urlMatchList, function(err, resp, body) {
+      request(urlMatchList, (err, resp, body) => {
         if(err) {
 
-          res.render('index', {summonerName: null, matchList:null, error: 'Error, try again'});
-        
+          res.render('home', {summonerName: null, matchList:null, error: 'Error, try again'});
+
         } else {
 
           let jsonBody = JSON.parse(body);
+
+          if(jsonBody.status.status_code >= 400) {
+            res.render('home', {error: "could not find summoner name"});
+          }
+
+          res.redirect(`/${summName}`); //last added, going through api tutorial
+
           let gameIds = [];
           let matchDetails = [];
 
@@ -45,21 +54,22 @@ app.post('/', function(req, res) {
           }
 
           for(i=0; i<gameIds.length; i++) {
-            let urlMatchDetails = `https://oc1.api.riotgames.com/lol/match/v4/matches/${gameIds[i]}?api_key=${apiKey}}`;
-
-
+            let urlMatchDetails = `${urlHost}/lol/match/v4/matches/${gameIds[i]}?api_key=${apiKey}}`;
           }
 
-          res.render('index', {summonerName: summName, matchList: gameIds, error: null});
-          
+          res.render('home', {summonerName: summName, matchList: gameIds, error: null});
+
         }
       });
     }
   });
 });
 
+app.get("/:summName", (req, res) => {
+
+});
 
 
-app.listen(3000, function() {
+app.listen(3000, () => {
   console.log('listening on 3000');
 });
